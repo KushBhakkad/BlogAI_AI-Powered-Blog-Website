cfd86cb8d5726a18e39fce8c95ce481e4297bced
KushBhakkad <kushbhakkad@gmail.com>
2024-07-05 18:16:37 +0530

    Connect to Clever Cloud

--- index.js
+++ index.js
@@ -36,11 +36,11 @@ const port = 8080;
 const saltRounds = 10;
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.POSTGRESQL_ADDON_USER,
+  host: process.env.POSTGRESQL_ADDON_HOST,
+  database: process.env.POSTGRESQL_ADDON_DB,
+  password: process.env.POSTGRESQL_ADDON_PASSWORD,
+  port: process.env.POSTGRESQL_ADDON_PORT,
 });
 db.connect();
 
