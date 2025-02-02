1072bb9ecd1bace7a164a7d25a10afcdfa322eb6
KushBhakkad <kushbhakkad@gmail.com>
2025-02-02 17:29:33 +0530

    New commit

--- services/textCorrectionService.js
+++ services/textCorrectionService.js
@@ -1,20 +1,11 @@
 import { retext } from 'retext';
 import retextSpell from 'retext-spell';
-import dictionary from 'dictionary-en';
+import dictionaryEn from 'dictionary-en';
 
 export const correctText = async (inputText) => {
     try {
-        // Load dictionary asynchronously
-        const dictionaryEn = await new Promise((resolve, reject) => {
-            dictionary((err, dict) => {
-                if (err) reject(err);
-                else resolve(dict);
-            });
-        });
-
-        // Apply spelling correction
         const file = await retext()
-            .use(retextSpell, { dictionary: dictionaryEn })
+            .use(retextSpell, { dictionary: dictionaryEn }) // Use dictionaryEn directly
             .process(inputText);
 
         let correctedText = inputText;
