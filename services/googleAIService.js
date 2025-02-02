c68c229cc1145bee62dcdee91c429bb4b01cbcf2
KushBhakkad <kushbhakkad@gmail.com>
2025-02-02 16:56:24 +0530

    Update prompt

--- services/googleAIService.js
+++ services/googleAIService.js
@@ -7,7 +7,8 @@ const genAI = new GoogleGenerativeAI(API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 export const generateBlog = async (title, category) => {
-    const prompt = `Write a detailed blog about ${title} in the ${category} category.`;
+    const prompt = `Generate a detailed and expansive single-paragraph blog content on the title of "${title}" within the "${category}" category. Explore various dimensions of "${title}". Avoid any points, headings, or structured format, presenting the information in a continuous, narrative paragraph. Ensure the content is as comprehensive and informative as possible while strictly adhering to the specified token size.`;
+
     try {
         const result = await model.generateContent(prompt);
         const response = await result.response;
