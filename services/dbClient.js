576461d2fecdf63c5abf563ab34953bfc88f9389
KushBhakkad <kushbhakkad@gmail.com>
2025-04-13 15:19:01 +0530

    Trying to debug the websocket error

--- services/dbClient.js
+++ services/dbClient.js
@@ -1,9 +1,15 @@
 // File: /services/dbClient.js
 
-import { Pool } from '@neondatabase/serverless';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import dotenv from 'dotenv';
+import ws from 'ws';
+import fetch from 'node-fetch';
 
 dotenv.config();
+
+// Fix for WebSocket errors
+neonConfig.webSocketConstructor = ws;
+neonConfig.fetch = fetch;
 
 const db = new Pool({ connectionString: process.env.DATABASE_URL });
 
